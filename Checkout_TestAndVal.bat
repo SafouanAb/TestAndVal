468dd249ce06a6d8e3ade7cbafb6e2792764cdd5
Safouan <Safouan.Aboulaaguig@v2c2.at>
2023-09-15 20:27:44 +0200

    Checkout_TestVal_Script

---
+++ Checkout_TestAndVal.bat
@@ -0,0 +1,15 @@
+@echo off
+:: Define the repository URL and target folder
+set repositoryUrl=https://github.com/SafouanAb/TestAndVal.git
+set targetFolder=C:\repo
+
+:: Check if the target folder exists; if not, create it
+if not exist "%targetFolder%" (
+    mkdir "%targetFolder%"
+)
+
+:: Change the current working directory to the target folder
+cd /d "%targetFolder%"
+
+:: Clone the Git repository
+git clone "%repositoryUrl%"
